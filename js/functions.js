d955a1837d0be62b24b902d902c93596007c9375
Luiza Leahu <leahuluiza@gmail.com>
2023-03-12 08:01:33 +0100

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -21,29 +21,30 @@ const isPalindrom = (string) => {
 isPalindrom('ДовОд');
 
 const extractNumber = (string) => {
-if (typeof string === 'number') {
-  return string;
-}
+  if (typeof string === 'number') {
+    return string;
+  }
 
   let result = '';
 
-for( let i = 0; i < string.length; i++) {
-if (!Number.isNaN(parseInt(string.at(i), 10))) {
-  result += string.at(i); }
-}
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
 
-return parseInt(result, 10);
+  return parseInt(result, 10);
 }
 
 extractNumber('1 кефир, 0.5 батона');
 
 
 const myPadStart = (string, minLength, pad) => {
-const actualPad = minLength - string.length
-if (actualPad <= 0) {
-  return string;
-}
-return pad.slice (0, actualPad % pad.length) + pad.repeat(actualPad/pad.length) + string
+  const actualPad = minLength - string.length
+  if (actualPad <= 0) {
+    return string;
+  }
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string
 }
 myPadStart('q', 4, 'we');
 
@@ -52,7 +53,7 @@ const newPadStart = (string, minLength, pad) => {
   let result = string;
   while (result.length < minLength) {
     const newResultLength = result.length + pad.length;
-    const actualPad = newResultLength <= minLength ? pad: pad.slice(0, minLength - newResultLength);
+    const actualPad = newResultLength <= minLength ? pad : pad.slice(0, minLength - newResultLength);
     result = actualPad + result;
   }
   return result;
