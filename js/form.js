b38d507a274649479998cb3bac14364ce962101e
Luiza Leahu <leahuluiza@gmail.com>
2023-04-08 10:01:52 +0200

    Правда или действие

--- js/form.js
+++ js/form.js
@@ -5,47 +5,14 @@ const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const cancelButton = document.querySelector('#upload-cancel');
 const fileField = document.querySelector('#upload-file');
-const hashtagField = document.querySelector('.text__hashtag');
+const hashtagField = document.querySelector('.text__hashtags');
 const commentField = document.querySelector('.text__description');
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
-  erorTextParent: 'img-upload__field-wrapper',
-  erorTextClass: 'img-upload__field-wrapper__eror',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__eror',
 });
-
-const openModal = () => {
-  overlay.classList.remove('hidden');
-  body.classList.add('modal-open');
-  document.addEventListener('keydown', onDocumentKeydown);
-};
-
-const closeModal = () => {
-  form.reset();
-  pristine.reset();
-  overlay.classList.add('hidden');
-  body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onDocumentKeydown);
-};
-
-const isTextFieldFocused = () =>
-  document.activeElement === hashtagField ||
-  document.activeElement === commentField;
-
-function onDocumentKeydown(evt) {
-  if (evt.key === 'Escape' && !isTextFieldFocused()) {
-    evt.preventDefault();
-    closeModal();
-  }
-}
-
-const onCancelButtonClick = () => {
-  closeModal();
-};
-
-const onFileImputChange = () => {
-  openModal();
-};
 
 const isValidTag = (tag) => VALID_SIMBOLS.test(tag);
 
@@ -70,11 +37,59 @@ pristine.addValidator(
   TAG_ERROR_TEXT
 );
 
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+const onCancelButtonClick = () => {
+  closeModal();
+};
+
+const onFileImputChange = () => {
+  openModal();
+};
+
 const onFormSubmit = (evt) => {
   evt.preventDefault();
   pristine.validate();
 };
 
-fileField.addEventListener('change', onFileImputChange);
-cancelButton.addEventListener('click', onCancelButtonClick);
-form.addEventListener('submit', onFormSubmit);
+function openModal() {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onDocumentKeydown);
+  cancelButton.addEventListener('click', onCancelButtonClick);
+  form.addEventListener('submit', onFormSubmit);
+}
+
+function closeModal() {
+  form.reset();
+  pristine.reset();
+  //hashtagField.reset();
+  //commentField.reset();
+
+  fileField.innerHTML = '';
+  commentField.innerHTML = '';
+  hashtagField.innerHTML = '';
+
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onDocumentKeydown);
+  cancelButton.removeEventListener('click', onCancelButtonClick);
+  form.removeEventListener('submit', onFormSubmit);
+}
+
+const activateUploader = () => {
+  fileField.addEventListener('change', onFileImputChange);
+};
+
+export default activateUploader;
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import getPhoto from './data.js';
+import activateUploader from './form.js';
 import renderThumbnails from './thumbnail.js';
 
 renderThumbnails(getPhoto());
+activateUploader();
